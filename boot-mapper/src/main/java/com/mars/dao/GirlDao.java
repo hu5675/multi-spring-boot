be5ba4a90d6b9da8455aa9da97a11e7e9b3662d4
mars <hytmars1989@126.com>
2017-11-20 14:11:10 +0800

    Mapper 之 Mybatis Annotation 注解的形式

--- boot-mapper/src/main/java/com/mars/dao/GirlDao.java
+++ boot-mapper/src/main/java/com/mars/dao/GirlDao.java
@@ -1,11 +1,18 @@
 package com.mars.dao;
 
 import com.mars.model.Girl;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface GirlDao {
+    @Select("select * from girl")
+    @Results({
+            @Result(column = "id",property = "id"),
+            @Result(column = "age",property = "age"),
+            @Result(column = "cup_size",property = "cupSize"),
+            @Result(column = "money",property = "money")
+    })
     List<Girl> list();
 }
