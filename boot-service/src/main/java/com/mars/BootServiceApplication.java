1adbc7e91f5d342e4950c6ed57c53d60e49cf91a
mars <hytmars1989@126.com>
2017-11-20 13:57:56 +0800

    分离service

---
+++ boot-service/src/main/java/com/mars/BootServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootServiceApplication.class, args);
+	}
+}
